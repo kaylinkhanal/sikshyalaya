8a10e981b6ca2d9391cc521a8efe20673a07ec8d
thekbbohara <ytkbzcraft@gmail.com>
2024-12-03 20:50:30 +0545

    return subject on creation

--- server/src/controllers/subject.js
+++ server/src/controllers/subject.js
@@ -2,21 +2,21 @@ const Section = require("../models/section");
 const Subject = require("../models/subject");
 
 const postNewSubjectInSectionId = async (req, res) => {
-  const {sectionId: section} = req.params
-  Subject.create({section, ...req.body })
-  res.send({msg: 'Subject created!!'})
+  const { sectionId: section } = req.params;
+  const sub = await Subject.create({ section, ...req.body });
+  console.log(sub);
+  res.send({ msg: "Subject created!!", subject: sub });
 };
 
-
-const getsSubjectBySectionId = async (req, res) => {
-
-};
+const getsSubjectBySectionId = async (req, res) => {};
 
 const getAllSubjects = async (req, res) => {
-  const data = await Subject.find()
-  res.send(data)
+  const data = await Subject.find();
+  res.send(data);
 };
 
-
-
-module.exports = {postNewSubjectInSectionId,getsSubjectBySectionId,getAllSubjects}
+module.exports = {
+  postNewSubjectInSectionId,
+  getsSubjectBySectionId,
+  getAllSubjects,
+};
