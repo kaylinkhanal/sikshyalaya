8c48f882cd314108bbe21af6d6a51d50f22f23c9
Kaylin Khanal <kyalinnop@gmail.com>
2024-12-18 06:56:29 +0545

    added assignment model

---
+++ client/src/components/custom-click.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const CustomClick = () => {
+  return (
+    <div>
+        <button onClick={()=>alert("click me")}></button>
+    </div>
+  )
+}
+
+export default CustomClick
