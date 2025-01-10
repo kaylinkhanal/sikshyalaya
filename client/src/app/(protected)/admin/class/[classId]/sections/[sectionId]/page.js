a2d84846955e3df3accdaa4b836ddc63e724260b
Aaditya Paudel <78781844+adityaspaudel@users.noreply.github.com>
2025-01-10 11:29:29 +0545

    duplicate column removed from table

--- client/src/app/(protected)/admin/class/[classId]/sections/[sectionId]/page.js
+++ client/src/app/(protected)/admin/class/[classId]/sections/[sectionId]/page.js
@@ -100,7 +100,6 @@ const Section = () => {
 								<th className="text-left px-2"> Is Verified</th>
 								<th className="text-left px-2">Father Name</th>
 								<th className="text-left px-2">Mother Name</th>
-								<th className="text-left px-2">Phone Number</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -112,7 +111,6 @@ const Section = () => {
 									<td className="px-2">{student.isVerified ? "Yes" : "No"}</td>
 									<td className="px-2">{student.fatherName}</td>
 									<td className="px-2">{student.motherName}</td>
-									<td className="px-2">{student.phoneNumber}</td>
 								</tr>
 							))}
 						</tbody>
